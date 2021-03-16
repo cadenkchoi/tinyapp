902309f3f6dbc94e919a83bc338481a437de9dbd
Caden Choi <cadenc.7@gmail.com>
2021-03-16 02:09:52 +0000

    redirect short URLs to actual websites

--- express_server.js
+++ express_server.js
@@ -4,9 +4,22 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+};
+
+function generateRandomString(n) {
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let newShort = '';
+  for (let i = 0; i < n; i++) {
+    const random = Math.floor(Math.random() * 62);
+   newShort += chars[random];
+  }
+  return newShort;
 };
 
 app.get("/", (req, res) => {
@@ -26,9 +39,25 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  const shortURL = generateRandomString(6);
+  const newURL = req.body.longURL;
+  urlDatabase[shortURL] = newURL;
+  res.redirect("/urls/" + shortURL);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
